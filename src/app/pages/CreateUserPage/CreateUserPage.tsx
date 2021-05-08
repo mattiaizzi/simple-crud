d09914a087ea3902651d18dc7c5cfa159e032789
Mattia Izzi <izzimattia@yahoo.it>
2021-05-08 15:10:21 +0200

    sostituzione con alert context

--- src/app/pages/CreateUserPage/CreateUserPage.tsx
+++ src/app/pages/CreateUserPage/CreateUserPage.tsx
@@ -1,36 +1,15 @@
-import { Paper, Snackbar, Typography } from '@material-ui/core';
-import React, { useReducer } from 'react';
-import Alert, { Color } from '@material-ui/lab/Alert';
+import { Paper, Typography } from '@material-ui/core';
+import React, { useContext } from 'react';
 import { User } from '../../models/user';
 import { UserForm } from '../../shared/UserForm/UserForm';
 import { useHistory } from 'react-router';
 import { saveUser } from '../../core/api/users';
 import { useMutation } from 'react-query';
-
-interface AlertState {
-  isOpen: boolean;
-  severity?: Color | undefined;
-  message?: string;
-}
-
-interface OpenPayload {
-  severity: Color | undefined;
-  message: string;
-}
-
-type AlertAction = { type: 'open'; payload: OpenPayload } | { type: 'close' };
-
-const alertReducer = (state: AlertState, action: AlertAction): AlertState => {
-  switch (action.type) {
-    case 'open':
-      return { isOpen: true, ...action.payload };
-    case 'close':
-      return { isOpen: false };
-  }
-};
+import { AlertContext } from '../../core/context/AlertContext/AlertContext';
 
 export const CreateUserPage: React.FC = () => {
-  const [{ isOpen, message, severity }, dispatch] = useReducer(alertReducer, { isOpen: false });
+  const { dispatch } = useContext(AlertContext);
+
   const history = useHistory();
   const saveUserMutation = useMutation(saveUser);
   const onSubmit = (user: User) => {
@@ -52,18 +31,12 @@ export const CreateUserPage: React.FC = () => {
         })
       );
   };
-  const handleClose = () => dispatch({ type: 'close' });
   return (
     <Paper elevation={3}>
       <Typography style={{ padding: '0.5em' }} variant="h6">
         AGGIUNGI UN NUOVO UTENTE
       </Typography>
       <UserForm onSubmit={onSubmit} />
-      <Snackbar open={isOpen} autoHideDuration={2000} onClose={handleClose}>
-        <Alert elevation={6} variant="filled" onClose={handleClose} severity={severity}>
-          {message}
-        </Alert>
-      </Snackbar>
     </Paper>
   );
 };
